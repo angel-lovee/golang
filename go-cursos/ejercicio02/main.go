8aade3a6f75d39bd13619ec6bacb6f12bc6d211c
Angel Pineda <89473377+angel-lovee@users.noreply.github.com>
2021-10-31 00:21:09 -0400

    Add files via upload

---
+++ go-cursos/ejercicio02/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+var numero int
+var texto string
+var status bool = true
+
+func main() {
+	
+	numero2, numero3, numero4, texto, status :=2, 5, 67, "Este es mi texto", false
+
+	var moneda float64 = 0
+	
+	numero2 = int(moneda)
+	texto=string
+
+	fmt.Println(numero2)
+	fmt.Println(numero3)
+	fmt.Println(numero4)
+	fmt.Println(texto)
+	fmt.Println(status)
+
+	MostrarStatus()
+}
+
+func MostrarStatus(){
+	fmt.Println(status)
+}
